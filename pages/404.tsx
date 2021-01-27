124229e78c55df53ee59ef96649f360a6598c2a7
artulloss <artulloss@gmail.com>
2021-01-27 16:18:16 -0500

    404 page stuffs

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function NotFound() {
   const [seconds, setSeconds] = useState(3);
@@ -7,14 +8,21 @@ export default function NotFound() {
     if (seconds > 1) {
       setSeconds((seconds) => seconds - 1);
     } else {
-      window.history.back(); // Redirect back
+      window.history.back();
     }
   }, 1000);
 
   return (
     <div className="w-100">
       <h1 className="text-center">We're sorry, but you seem lost...</h1>
-      <p className="text-center">Redirecting you back in {seconds}...</p>
+      <p className="text-center">
+        Attempting to redirect you back in {seconds}...
+        <hr />
+        If this fails, please close the window or navigate to the{" "}
+        <Link href="/login">
+          <a>login page</a>
+        </Link>
+      </p>
     </div>
   );
 }
